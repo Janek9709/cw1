963143627d786a01f4e6cc96e35c85f03e805ada
s18313 <s18313@LAB001-13>
2020-03-02 11:37:24 +0100

    init commit

---
+++ cw1-dobre/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw1_dobre
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+
+
+    }
+}
---
+++ cw1-dobre/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1_dobre
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            /*            int? tmp = null;
+                        double tmp2 = 2.0;
+                        string tmp3 = "abc";
+                        bool tmp4 = true;*/
+                        
+            //var tmp5 = "ala";//ale fajne 
+
+            //var newPerson = new Person { FirstName = "Jakub" };
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            var httpClient = new HttpClient();
+
+
+            var response = await httpClient.GetAsync(url);
+            
+            //status 2xx
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(htmlContent);
+
+                foreach(var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+
+
+        }
+    }
+}
